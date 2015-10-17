2677538fd4484ee84caf50948d73438cdd4138e1
Kyle Munz <kyle138@gmail.com>
2015-10-17 00:14:58 -0500

    Initial commit. Using AWS SDK for PHP V3 to send emails. Returns MessageId on successful sending. Requires sescredentials.php file first. (Not included)

---
+++ sdksendmail.php
@@ -0,0 +1,68 @@
+<?php
+// SES SendMail demo
+// Using AWS SDK for PHP V3
+// -KM
+
+require(dirname(__FILE__).'/include/aws/aws-autoloader.php');
+require(dirname(__FILE__).'/sescredentials.php');
+
+use Aws\Ses\SesClient;
+use Aws\Ses\Exception\SesException;
+
+// Initialize SES Credentials
+// Stored separately in sescredentials.php for security reasons
+// sescredentials.php are NOT kept in the git repo (see security reasons above)
+$credentials = new Aws\Credentials\Credentials($key,$secret);
+
+function Send_Mail($from,$fromH=NULL,$to=[],$cc=[],$bcc=[],$subject,$bodyH,$bodyT)
+{
+  // Initialization of initializing the initial variables, initially.
+  global $credentials;  //Considering the scope of things. Get it? Scope?
+  $fromAdrs=$from;
+  $sourceAdrs=isset($fromH)?$fromH.'<'.$fromAdrs.'>':$fromAdrs;
+  $toAdrs=$to;
+  $ccAdrs=$cc;
+  $bccAdrs=$bcc;
+  $Subject = [
+    'Charset' => 'UTF-8',
+    'Data' => $subject,
+  ];
+  $body = [
+    'Html' => [
+      'Charset' => 'UTF-8',
+      'Data' => $bodyH,
+    ],
+    'Text' => [
+      'Charset' => 'UTF-8',
+      'Data' => $bodyT,
+    ],
+  ];
+
+  //Create AWS connection and SesClient
+  $SesClient = new Aws\Ses\SesClient([
+    'version'   =>  'latest',
+    'region'    =>  'us-east-1',
+    'credentials' =>  $credentials
+  ]);
+
+  $result = $SesClient->sendEmail([
+    //'Destination' => $destination,
+    'Destination' => [
+      'BccAddresses' => $bccAdrs,
+      'CcAddresses' => $ccAdrs,
+      'ToAddresses' => $toAdrs,
+    ],
+    'Message' => [
+      'Body' => $body,
+      'Subject' => $Subject,
+    ],
+    'ReplyToAddresses' => [$fromAdrs],
+    'ReturnPath' => $fromAdrs,
+    'Source' => $sourceAdrs,
+  ]);
+//  echo "Result: $result\r\n";   //DEBUG
+//  echo "Result['MessageId']: ".$result['MessageId']."\r\n";   //DEBUG
+
+  return $result['MessageId'];
+}
+?>
---
+++ sdktest.php
@@ -0,0 +1,15 @@
+<?php
+require('sdksendmail.php');
+$time=date('H:i',time());
+$from = "AnSesValidated@email.com";
+$fromH = "A Human Readable Email Source";  //Optional
+$to = ['bounce@simulator.amazonses.com',]; //SES Bounce simulator Address for testing
+$cc = [];                               //to,cc,bcc addresses must be arrays
+$bcc = [];
+$subject = "Test mail $time sent through SES";
+$bodyH = "This message body contains <strong>HTML formatting</strong>. It can, <i>for example</i>, contain links like this one: <a class=\"ulink\" href=\"http://docs.aws.amazon.com/ses/latest/DeveloperGuide\" target=\"_blank\">Amazon SES Developer Guide</a>.";
+$bodyT = "This is the message body in text format. It contains no links and no formatting.";
+$result = Send_Mail($from,$fromH,$to,$cc,$bcc,$subject,$bodyH,$bodyT);
+echo "Email sending...\r\n";
+echo $result."\r\n";
+?>
